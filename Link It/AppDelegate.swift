f128f16956e3a6b4e311bdd418ad737a0e856620
Mirko Cukich <mirkocukich@gmail.com>
2016-11-01 23:18:19 -0500

    Added link icon graphic to status bar area and put in credit in window box for the graphic's author

--- Link It/AppDelegate.swift
+++ Link It/AppDelegate.swift
@@ -5,6 +5,9 @@
 //  Created by Mirko Cukich on 11/1/16.
 //  Copyright © 2016 Digital Mirko. All rights reserved.
 //
+// icon used color black, size 32 pixels, png format
+// icon credit: Eleonor Wang from flaticon.com
+// <div>Icons made by <a href="http://www.flaticon.com/authors/eleonor-wang" title="Eleonor Wang">Eleonor Wang</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 import Cocoa
 
@@ -20,13 +23,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Create Status Bar Item
         item = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
         // Title for status bar item
-        item?.title = "Link It!"
+//        item?.title = "Link It!"
         // When someone clicks on name run this function
-        item?.action = #selector(AppDelegate.linkIt)
+//        item?.action = #selector(AppDelegate.linkIt)
+        
+        // loading icon graphic
+        item?.image = NSImage(named: "link")
+        
         
         let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Link It!", action: #selector(AppDelegate.linkIt), keyEquivalent: "L"))
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: "Q"))
+        // empty keyEquivalent makes app look cleaner instead of short cuts being shown
+        menu.addItem(NSMenuItem(title: "Link It!", action: #selector(AppDelegate.linkIt), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
         
         // assign into status bar
         item?.menu = menu
